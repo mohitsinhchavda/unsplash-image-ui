2fd98e7a4fcede3d61afb967aadb01ebc03061b4
mohitsinhchavda <mohit.chavda1997@gmail.com>
2021-06-25 11:20:14 +0530

    total for search image being extracted from json and not from header

--- src/pages/api/searchImage.js
+++ src/pages/api/searchImage.js
@@ -4,8 +4,8 @@ export default async function handler(req, res) {
   if(req.method === "GET"){
     try{
       const apiRes = await commoUnsplashFetchUtilFunc("search", req.query);
-      const {results : finalRes} = await apiRes.json();
-      res.status(200).json({resultsArr : finalRes, total : apiRes.headers.get("x-total")});
+      const {results : finalRes, total} = await apiRes.json();
+      res.status(200).json({resultsArr : finalRes, total : Math.ceil(Number(total)/10)});
     }
     catch{
       res.status(400).json({
